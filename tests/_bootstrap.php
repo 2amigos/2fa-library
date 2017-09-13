73a965be61a95b8eab7b9220eeb1e9112eb4c913
Antonio Ramirez <amigo.cobos@gmail.com>
2017-09-13 18:09:08 +0200

    trying to fix travis

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,2 +1,13 @@
 <?php
-require __DIR__ . '/../vendor/autoload.php';
+// Search for autoload, since performance is irrelevant and usability isn't!
+$dir = __DIR__ . '/..';
+while (!file_exists($dir . '/vendor/autoload.php')) {
+    if ($dir == dirname($dir)) {
+        throw new \Exception('Failed to locate autoload.php');
+    }
+    $dir = dirname($dir);
+}
+
+$vendor = $dir . '/vendor';
+
+require_once $vendor . '/autoload.php';
